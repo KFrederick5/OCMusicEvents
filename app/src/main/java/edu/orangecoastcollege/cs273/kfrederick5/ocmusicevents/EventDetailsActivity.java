e6663045df8ab878f07f7132f58c942648772162
Keegan Frederick <tefsuncle@hotmail.com>
2016-10-04 11:53:36 -0700

    Finished with added java docing

--- app/src/main/java/edu/orangecoastcollege/cs273/kfrederick5/ocmusicevents/EventDetailsActivity.java
+++ app/src/main/java/edu/orangecoastcollege/cs273/kfrederick5/ocmusicevents/EventDetailsActivity.java
@@ -15,6 +15,10 @@ import java.io.InputStream;
 
 public class EventDetailsActivity extends AppCompatActivity {
 
+    /**
+     * Created three private view variables to relate to the coinciding
+     * xml view
+     */
     private ImageView eventImageView;
     private TextView eventTitleText;
     private TextView eventDetailText;
@@ -22,21 +26,37 @@ public class EventDetailsActivity extends AppCompatActivity {
     //In order to use AssetManager, need to know context
     private Context context = (Context) this;
 
+    /**
+     * Creates a view after one of the list items have been clicked and
+     * displays the details of the item selected.
+     * @param savedInstanceState calls for any recent saved information.
+     */
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_event_details);
 
+        /**
+         * Received the intent from the EventListActivity and assigned them
+         * to string variable values
+         */
         Intent detailsIntent = getIntent();
         String title = detailsIntent.getStringExtra("Title");
         String detail = detailsIntent.getStringExtra("Detail");
         String imageFileName = title.replace(" ","") + ".jpeg";
 
+        /**
+         * Hooked this controllers variables to the xml views.
+         */
         eventImageView = (ImageView) findViewById(R.id.eventImageView);
         eventTitleText = (TextView) findViewById(R.id.eventTitleTextView);
         eventDetailText = (TextView) findViewById(R.id.eventDetailsTextView);
 
-        eventTitleText = ;
+        /**
+         * Set the text views in the related xml to the string values set earlier
+         */
+        eventTitleText.setText(title);
+        eventDetailText.setText(detail);
 
         //Load image from Assets folder using AssetManager class
         AssetManager am = context.getAssets();
--- app/src/main/java/edu/orangecoastcollege/cs273/kfrederick5/ocmusicevents/EventListActivity.java
+++ app/src/main/java/edu/orangecoastcollege/cs273/kfrederick5/ocmusicevents/EventListActivity.java
@@ -9,18 +9,29 @@ import android.widget.ListView;
 
 public class EventListActivity extends ListActivity {
 
+    /**
+     * Displays a list of upcoming concert events by the artist names.
+     * These list items are clickable
+     * @param savedInstanceState calls for any saved information.
+     */
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
         //Set adapter (which binds the ListView with the data in MusicEvent.java
         // Since Data is in array, we use an ArrayAdapter:
-
         setListAdapter(new ArrayAdapter<>(this,android.R.layout.simple_list_item_1,MusicEvent.titles));
 
         //Will cause crash: setContentView(R.layout.activity_event_list);
     }
 
+    /**
+     * Makes the items in the list react when clicked on.
+     * @param l relates to the list view
+     * @param v relates to the view being used
+     * @param pos relates to the position of the users input
+     * @param id relates to the id of the users input
+     */
     @Override
     protected void onListItemClick(ListView l, View v, int pos, long id)
     {
